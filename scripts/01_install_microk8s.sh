5a5acb36d8aee2c0c24b44ef7dc775afeb2a0a4a
hsm207 <hsm207@users.noreply.github.com>
2021-06-15 15:49:36 +0000

    Redo nginx controller installation

--- scripts/01_install_microk8s.sh
+++ scripts/01_install_microk8s.sh
@@ -7,12 +7,27 @@ sudo chown -f -R $USER ~/.kube
 
 sudo microk8s enable dns storage
 
+# alias for kubectl
+sudo snap alias microk8s.kubectl kubectl
+
+
+# install helm
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
 # add NGINX Ingress Controller
+# install's into the default namespace
+microk8s kubectl config view --raw >~/.kube/config
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm install ingress-nginx ingress-nginx/ingress-nginx
+
 # from: https://microk8s.io/docs/addon-ingress
 sudo microk8s enable ingress
 
-# To view the pods in this ingress controller:
-# microk8s kubectl get pods -n ingress \
-#   -l name=nginx-ingress-
+# To check the ingress controller version:
+POD_NAME=$(microk8s kubectl get pods -l app.kubernetes.io/name=ingress-nginx -o jsonpath='{.items[0].metadata.name}')
+
+microk8s kubectl exec -it $POD_NAME -- /nginx-ingress-controller --version
+
 
 # official docs: https://kubernetes.github.io/ingress-nginx/
