5877d3cfefe371aaf963375779b870a6c36d9dce
hsm207 <hsm207@users.noreply.github.com>
2021-06-15 12:48:51 +0000

    How to view pods in ingress controller

--- scripts/01_install_microk8s.sh
+++ scripts/01_install_microk8s.sh
@@ -10,3 +10,7 @@ sudo microk8s enable dns storage
 # add NGINX Ingress Controller
 # from: https://microk8s.io/docs/addon-ingress
 sudo microk8s enable ingress
+
+# To view the pods in this ingress controller:
+# microk8s kubectl get pods -n ingress \
+#   -l name=nginx-ingress-microk8s
