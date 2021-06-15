b4bc782404267b8c41b06a1bb9f7cecdcf65f797
hsm207 <hsm207@users.noreply.github.com>
2021-06-15 17:28:37 +0000

    Add reference

--- ingress_demo/ingress_demo.sh
+++ ingress_demo/ingress_demo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# source: https://matthewpalmer.net/kubernetes-app-developer/articles/kubernetes-ingress-guide-nginx-example.html
+
 kubectl apply -f ./apple.yml
 kubectl apply -f ./banana.yml
 kubectl apply -f ./ingress.yml
